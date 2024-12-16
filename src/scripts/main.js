0c042e52410e6a5dad8bdaa3b591e2ba9b32dfd0
Yura <yuradziavoruk5@gmail.com>
2024-12-16 19:27:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent;
+  li.replaceChild(span, li.firstChild);
+
+  span.addEventListener('click', (e) => {
+    const children = li.querySelector('ul');
+
+    if (children) {
+      children.style.display =
+        children.style.display === 'none'
+          ? (children.style.display = 'block')
+          : (children.style.display = 'none');
+    }
+  });
+});
